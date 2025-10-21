20674508002d98f8a1cd63229e76f048df3e08bd
Jordy Moreno Arias <yordimorenoarias.11@gmail.com>
2025-10-21 12:20:08 -0400

    Add user repository with pagination and query support
    Introduces IUserRepository interface and its implementation UserRepository, providing methods for adding, updating, and retrieving users, including paginated and filtered queries. Adds PagedResult<T> generic DTO for paginated responses and QueryUserParameters DTO for user query filtering. Updates project file to remove explicit Repositories folder include.

---
+++ ClinicManagerAPI/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using ClinicManagerAPI.Models.DTOs.Generic;
+using ClinicManagerAPI.Models.Entities;
+using EcommerceAPI.Models.DTOs.User;
+
+namespace ClinicManagerAPI.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<UserEntity> AddUser(UserEntity user);
+        Task<UserEntity?> GetUserByEmail(string email);
+        Task<UserEntity?> GetUserById(int id);
+        Task<UserEntity?> GetUserByUsername(string username);
+        Task<PagedResult<UserEntity>> GetUsers(QueryUserParameters parameters);
+        Task<UserEntity> UpdateUser(UserEntity user);
+    }
+}
