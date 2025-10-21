01f8ff522ae0ee0c7b9b24876a5febd9d02bd327
Jordy Moreno Arias <yordimorenoarias.11@gmail.com>
2025-10-21 11:50:38 -0400

    Add JWT authentication and token generation service
    Upgraded target framework to .NET 9.0 and added Microsoft.AspNetCore.Authentication.JwtBearer package. Introduced JwtOptions configuration, UserGenerateTokenDto, and implemented IJwtService and JwtService for JWT token generation. Configured JWT authentication in Program.cs to enable secure user authentication and authorization.

--- ClinicManagerAPI/Program.cs
+++ ClinicManagerAPI/Program.cs
@@ -1,5 +1,9 @@
 using ClinicManagerAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +16,59 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// JWT Authentication
+var jwtKey = builder.Configuration["JWT:KEY"];
+
+if (string.IsNullOrEmpty(jwtKey))
+{
+    throw new Exception("JWT:KEY is not configured");
+}
+
+var jwtIssuer = builder.Configuration["JWT:ISSUER"];
+
+if (string.IsNullOrEmpty(jwtIssuer))
+{
+    throw new Exception("JWT:ISSUER is not configured");
+}
+
+var jwtAudience = builder.Configuration["JWT:AUDIENCE"];
+
+if (string.IsNullOrEmpty(jwtAudience))
+{
+    throw new Exception("JWT:AUDIENCE is not configured");
+}
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = jwtIssuer,
+        ValidAudience = jwtAudience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+    };
+
+    options.Events = new JwtBearerEvents
+    {
+        OnAuthenticationFailed = context =>
+        {
+            var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+            logger.LogError($"Authentication failed: {context.Exception.Message}");
+            return Task.CompletedTask;
+        },
+        OnTokenValidated = context =>
+        {
+            var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("Token validated successfully.");
+            return Task.CompletedTask;
+        }
+    };
+});
 
 var app = builder.Build();
 
