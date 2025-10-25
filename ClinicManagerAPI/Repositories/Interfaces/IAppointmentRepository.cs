eb89ed36fdf83924ebae17a4bb7c51954cd5ac58
Jordy Moreno Arias <yordimorenoarias.11@gmail.com>
2025-10-25 13:57:30 -0400

    Add appointment repository with filtering and sorting
    Introduces the AppointmentRepository and its interface for managing appointments, including methods for CRUD operations and paginated, filterable, and sortable queries. Adds QueryAppointmentParameters DTO for flexible querying and an AppointmentSortBy enum for sorting options.

---
+++ ClinicManagerAPI/Repositories/Interfaces/IAppointmentRepository.cs
@@ -0,0 +1,47 @@
+﻿using ClinicManagerAPI.Models.DTOs.Appointment;
+using ClinicManagerAPI.Models.DTOs.Generic;
+using ClinicManagerAPI.Models.Entities;
+
+namespace ClinicManagerAPI.Repositories.Interfaces
+{
+    /// <summary>
+    /// Repository for managing appointment data.
+    /// </summary>
+    public interface IAppointmentRepository
+    {
+        /// <summary>
+        /// Adds a new appointment.
+        /// </summary>
+        /// <param name="appointment"></param>
+        /// <returns> A task representing the asynchronous operation.</returns>
+        Task AddAppointment(AppointmentEntity appointment);
+
+        /// <summary>
+        /// Deletes an appointment.
+        /// </summary>
+        /// <param name="appointment"></param>
+        /// <returns> A task representing the asynchronous operation.</returns>
+        Task DeleteAppointment(AppointmentEntity appointment);
+
+        /// <summary>
+        /// Gets an appointment by its ID.
+        /// </summary>
+        /// <param name="appointmentId"></param>
+        /// <returns> The appointment entity if found; otherwise, null.</returns>
+        Task<AppointmentEntity?> GetAppointmentById(int appointmentId);
+
+        /// <summary>
+        /// Gets a paged list of appointments based on query parameters.
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <returns>Paged result of appointment entities.</returns>
+        Task<PagedResult<AppointmentEntity>> GetAppointments(QueryAppointmentParameters parameters);
+
+        /// <summary>
+        /// Updates an existing appointment.
+        /// </summary>
+        /// <param name="appointment"></param>
+        /// <returns> A task representing the asynchronous operation.</returns>
+        Task UpdateAppointment(AppointmentEntity appointment);
+    }
+}
